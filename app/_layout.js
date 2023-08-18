e08e9ccee9e1a562878bafcce101b73757cd37e9
s-nashad2020 <shaheernashad@gmail.com>
2023-08-18 15:50:54 +0500

    Initial Commit

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,29 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "home",
+};
 
 const Layout = () => {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/38577---.jpg";
+
+export default {
+  profile,
+};
